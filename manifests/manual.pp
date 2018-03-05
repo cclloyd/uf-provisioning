774676fc305ff3ce69821a6577bc6537a467b799
Michael <cclloyd9786@gmail.com>
2018-03-05 00:54:10 -0500

    updated

--- manifests/manual.pp
+++ manifests/manual.pp
@@ -1,44 +1,45 @@
  
-
-Optional[String] $sitename
-
-
-package { 'tree':
-	ensure => installed,
-}	
-package { 'nginx':
-	ensure => installed,
-}	
-
-service { 'nginx':
-	ensure     => stopped,
-	require => Package['nginx'],
-}
+class cclloydprovisioning {
+	Optional[String] $sitename
 
 
-#file { "/etc/nginx/sites-available/cclloyd.com.conf":
-#	ensure => "present",
-#	source  => "puppet:///modules/ufprovisioning/conf/cclloyd.com.conf",
-#	owner   => 'root',
-#	group   => 'root',
-#	mode    => '0755',
-#	require => Package['nginx']
-#}
+	package { 'tree':
+		ensure => installed,
+	}	
+	package { 'nginx':
+		ensure => installed,
+	}	
 
-file { "/etc/nginx/sites-available/testtemplate.conf":
-	ensure => "present",
-	source  => "puppet:///modules/ufprovisioning/conf/testtemplate.conf",
-	content => epp('site_name', $sitename)
-}
+	service { 'nginx':
+		ensure     => stopped,
+		require => Package['nginx'],
+	}
 
-file { '/etc/nginx/sites-enabled/cclloyd.com.conf':
-	ensure => 'link',
-	target => '/etc/nginx/sites-available/cclloyd.com.conf',
-}
-service { 'nginx':
-	ensure     => running,
-	enable     => true,
-	hasstatus  => true,
-	hasrestart => true,
-	require => Package['nginx'],
+
+	#file { "/etc/nginx/sites-available/cclloyd.com.conf":
+	#	ensure => "present",
+	#	source  => "puppet:///modules/ufprovisioning/conf/cclloyd.com.conf",
+	#	owner   => 'root',
+	#	group   => 'root',
+	#	mode    => '0755',
+	#	require => Package['nginx']
+	#}
+
+	file { "/etc/nginx/sites-available/testtemplate.conf":
+		ensure => "present",
+		source  => "puppet:///modules/ufprovisioning/conf/testtemplate.conf",
+		content => epp('site_name', $sitename)
+	}
+
+	file { '/etc/nginx/sites-enabled/cclloyd.com.conf':
+		ensure => 'link',
+		target => '/etc/nginx/sites-available/cclloyd.com.conf',
+	}
+	service { 'nginx':
+		ensure     => running,
+		enable     => true,
+		hasstatus  => true,
+		hasrestart => true,
+		require => Package['nginx'],
+	}
 }
