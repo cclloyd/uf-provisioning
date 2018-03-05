56df1bc7e21a2d7e280e051fc6fc3811b025a20c
Michael <cclloyd9786@gmail.com>
2018-03-05 00:38:59 -0500

    updated

--- manifests/manual.pp
+++ manifests/manual.pp
@@ -1,6 +1,21 @@
-# == Class: ufprovisioning::install
 
 package { 'tree':
 	ensure => installed,
 }	
+package { 'nginx':
+	ensure => installed,
+}	
 
+file { "/etc/nginx/sites-available/cclloyd.com.conf":
+	ensure => "present",
+	source  => "puppet:///modules/ufprovisioning/conf/cclloyd.com.conf",
+	owner   => 'root',
+	group   => 'root',
+	mode    => '0755',
+	require => Package['nginx']
+}
+
+file { '/etc/nginx/sites-enabled/cclloyd.com.conf':
+	ensure => 'link',
+	target => '/etc/nginx/sites-available/cclloyd.com.conf',
+}
