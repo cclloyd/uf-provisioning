e39f2a7f68b0874df1b0ab148d3895001a7da423
Michael <cclloyd9786@gmail.com>
2018-03-05 00:52:03 -0500

    updated

--- manifests/manual.pp
+++ manifests/manual.pp
@@ -1,3 +1,7 @@
+ 
+
+Optional[String] $sitename
+
 
 package { 'tree':
 	ensure => installed,
@@ -6,16 +10,35 @@ package { 'nginx':
 	ensure => installed,
 }	
 
-file { "/etc/nginx/sites-available/cclloyd.com.conf":
+service { 'nginx':
+	ensure     => stopped,
+	require => Package['nginx'],
+}
+
+
+#file { "/etc/nginx/sites-available/cclloyd.com.conf":
+#	ensure => "present",
+#	source  => "puppet:///modules/ufprovisioning/conf/cclloyd.com.conf",
+#	owner   => 'root',
+#	group   => 'root',
+#	mode    => '0755',
+#	require => Package['nginx']
+#}
+
+file { "/etc/nginx/sites-available/testtemplate.conf":
 	ensure => "present",
-	source  => "puppet:///modules/ufprovisioning/conf/cclloyd.com.conf",
-	owner   => 'root',
-	group   => 'root',
-	mode    => '0755',
-	require => Package['nginx']
+	source  => "puppet:///modules/ufprovisioning/conf/testtemplate.conf",
+	content => epp('site_name', $sitename)
 }
 
 file { '/etc/nginx/sites-enabled/cclloyd.com.conf':
 	ensure => 'link',
 	target => '/etc/nginx/sites-available/cclloyd.com.conf',
+}
+service { 'nginx':
+	ensure     => running,
+	enable     => true,
+	hasstatus  => true,
+	hasrestart => true,
+	require => Package['nginx'],
 }
