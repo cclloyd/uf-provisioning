dafa2136624ae651611b3f07545d2278b05784a7
Michael <cclloyd9786@gmail.com>
2018-03-06 00:01:26 -0500

    updated

--- manifests/config.pp
+++ manifests/config.pp
@@ -29,9 +29,9 @@ class ufprovisioning::config {
 		server_name 	=>	[$site_name],
 		www_root 		=>	"/var/www/${site_name}/public",
 		listen_port 	=>	80,
-		#ssl 			=>	true,
-		#ssl_cert		=>	"/etc/letsencrypt/live/${site_name}/fullchain.pem",
-		#ssl_key			=>	"/etc/letsencrypt/live/${site_name}/privkey.pem",
+		ssl 			=>	true,
+		ssl_cert		=>	"/etc/letsencrypt/live/${site_name}/fullchain.pem",
+		ssl_key			=>	"/etc/letsencrypt/live/${site_name}/privkey.pem",
 		ssl_port		=>	443,
 	}
 	
