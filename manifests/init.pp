45cda4981eb55d9d534b93d050bc7a389d89dd8d
Michael <cclloyd9786@gmail.com>
2018-03-05 19:02:50 -0500

    updated

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,8 @@ class ufprovisioning {
 	
 	
 	#Boolean $webserver_manage		= true,
-	$webserver_manage				= $::ufprovisioning::params::webserver_manage
-	$site_name						= $::ufprovisioning::params::site_name
+	Optional[Boolean] $webserver_manage				= $::ufprovisioning::params::webserver_manage
+	Optional[String] $site_name						= $::ufprovisioning::params::site_name
 	#$daemon_user					= $::nginx::params::daemon_user
 
 	include ufprovisioning::params
