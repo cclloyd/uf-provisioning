bdbc0a4e2a3e72faa5b16fc5a99f3da550e24252
Michael <cclloyd9786@gmail.com>
2018-03-05 20:58:09 -0500

    updated

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,24 +1,12 @@
 class ufprovisioning (
 	
 	
-	#Boolean $webserver_manage		= true,
 	$webserver_manage				= $::ufprovisioning::params::webserver_manage,
-	$site_name						= $::ufprovisioning::params::site_name
-	#$daemon_user					= $::nginx::params::daemon_user
-
-	#include ufprovisioning::params
-	
-	#include ufprovisioning::install
-	#include ufprovisioning::config
-	#include ufprovisioning::service
+	$site_name						= $::ufprovisioning::params::site_name,
 	
 
 ) inherits ::ufprovisioning::params {
 
-	#contain '::nginx::package'
-	#contain '::nginx::config'
-	#contain '::nginx::service'
-	
 	contain ufprovisioning::install
 	contain ufprovisioning::config
 	contain ufprovisioning::service
@@ -26,6 +14,6 @@ class ufprovisioning (
 	# Allow the end user to establish relationships to the "main" class
 	# and preserve the relationship to the implementation classes through
 	# a transitive relationship to the composite class.
-	#Class['::nginx::package'] -> Class['::nginx::config'] ~> Class['::nginx::service']
-	#Class['::nginx::package'] ~> Class['::nginx::service']
+	Class['::ufprovisioning::install'] -> Class['::ufprovisioning::config'] ~> Class['::ufprovisioning::service']
+	Class['::ufprovisioning::install'] ~> Class['::ufprovisioning::service']
 }
