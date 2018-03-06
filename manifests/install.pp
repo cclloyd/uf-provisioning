65fc00d36b806873bb852e76963af97ea33786ea
Michael <cclloyd9786@gmail.com>
2018-03-05 22:25:55 -0500

    updated

--- manifests/install.pp
+++ manifests/install.pp
@@ -11,8 +11,8 @@
 class ufprovisioning::install {
 	
 	class { 'nginx': 
-		manage_repo => true,
-		package_source => 'nginx-mainline'
+		#manage_repo => true,
+		#package_source => 'nginx-mainline'
 	}
 	
 	package { 'tree':
