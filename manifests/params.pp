1902bc40749e2e615d93726129db2701bf9d88f0
Michael <cclloyd9786@gmail.com>
2018-03-05 21:33:13 -0500

    updated

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,13 +2,13 @@
 class ufprovisioning::params {
 
 	#$site_name = "notarealsite"
-	$_module_parameters = {
-		'site_name'				=> "site-name",
-		'webserver_manage'		=> true,
-	}
+	#$_module_parameters = {
+	#	'site_name'				=> "site-name",
+	#	'webserver_manage'		=> true,
+	#}
 	
-	$site_name = "webserver.test"
-	$webserver_manage = true
+	$site_name # = "webserver.test"
+	$webserver_manage # = true
 	
 	#$_module_parameters = merge($_module_defaults, $_module_os_overrides)
 	
