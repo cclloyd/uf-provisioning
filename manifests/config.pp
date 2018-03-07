37d419fc9aaabf34c89509ebcef56d9511fd0e9c
Michael <cclloyd9786@gmail.com>
2018-03-06 22:26:34 -0500

    updated

--- manifests/config.pp
+++ manifests/config.pp
@@ -254,7 +254,7 @@ class ufprovisioning::config {
 	
 	grafana_user { 'username':
 		grafana_url       => 'http://localhost:3000',
-		grafana_api_path  => '/grafana/api'
+		grafana_api_path  => '/grafana/api',
 		grafana_user      => 'admin',
 		grafana_password  => 'password',
 		full_name         => 'Admin User',
