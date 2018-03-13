b977d70dc5b78fba3759dc416bb9620585c50157
Michael <cclloyd9786@gmail.com>
2018-03-12 23:54:22 -0400

    updated

--- manifests/git.pp
+++ manifests/git.pp
@@ -90,6 +90,11 @@ class ufprovisioning::git {
 		line => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDFoyTIxhjZNIDocz2Pw4TlezAcwNXuEyo7oEfr+qsY8UWPaXKH8DcVXBTdPh2fcCYfzYJ98Ef8DhdvjMxpvSTw+3xETlC55qbHZdAkfNnZUZRaGmPZysy+jeErQEN2q6Flb6MX2ozEiQKwR4um1XaY1USW8dXO/bgVZ2EIAoE7Zu9wy2nqoHACH6phPVU6Wlg6w2jYQkNwxb+5FUo9mDREwshy7LwR5s8jxnsUJkEhC7ZImdEX9ZexAhfHkee9VWWkepxHtXE8D18uXEn/w8GCXwqlxJ6D5AtBGSAtSKcUIfsS63PKs8aVHoFX1eYX1UOHUlsNZuY6tr7/8Fq/Yr85 Michael@Celestial Metarch'
 	}
 	
+	file_line { 'keys_git_git':
+		path => '/home/git/.ssh/authorized_keys',
+		line => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCYnyEPyq9itlw6tbw3Ptlj9I55eTKJ1m4MifRh8TEhCU53AQPmkuwH3X4/cdDN9KiNx4w4vJuDmqn9WVmmAVtqVwhNPTHJxxMVPsZXvV9uTT2XAhj8oibsfrFTQK1etFQEWhhnazawc12YXGt+sjixvoP6r6IfPyQdN9JVf2SxsGh/5YWtTHUcHFQ73UHwfbt4PXHnXE8weqeqjTFC5/zfAV0nAutsP6W8YVrTYRB7ZDdt3so6luzP38Lo8ZyQZT08uagteIsVDVRlwKktx5mIF/YX8KGpqnRoIH69b7moGOpoYDa5zorUFEwi+1lte3RfTbMt3xeVDIW/xyc1evRf git@cclloyd.com'
+	}
+	
 	
 	
 	
