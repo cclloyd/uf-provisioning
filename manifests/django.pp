b9271c19dc165d996a0a5b2c028338586e3a2315
Michael <cclloyd9786@gmail.com>
2018-03-25 19:03:24 -0400

    updated

---
+++ manifests/django.pp
@@ -0,0 +1,52 @@
+class ufprovisioning::django {
+
+	
+	
+	######################################################
+	###  Bash Customization
+	######################################################
+	
+	package { 'virtualenv':
+		ensure 		=> 	installed,
+		provider	=>	'apt',
+	}	
+	
+	package { 'virtualenvwrapper':
+		ensure 		=> 	installed,
+		provider	=>	'apt',
+	}
+	
+	package { 'python3-dev':
+		ensure 		=> 	installed,
+		provider	=>	'apt',
+	}
+	
+	package { 'uwsgi':
+		ensure 		=> 	installed,
+		provider	=>	'apt',
+	}
+	
+	package { 'setuptools':
+		ensure 		=> 	installed,
+		provider	=>	'pip3',
+	}
+	
+	package { 'wheel':
+		ensure 		=> 	installed,
+		provider	=>	'pip3',
+	}
+	
+	package { 'django':
+		ensure 		=> 	installed,
+		provider	=>	'pip3',
+	}
+	
+	
+	
+	vcsrepo { "/home/git/${site_name}_django":
+		ensure  	=> 	present,
+		provider	=> 	git,
+		source  	=> 	'git@bitbucket.org:cclloydcom/websrd-django.git',
+		user		=>	'git',
+	}
+}
